c417f8b6b68b121a2e2f94cae489fafcdcb72ad1
Gaurav Bajpai <gaurav.tcs15@gmail.com>
2020-10-03 12:45:58 +0530

    added cards compoenet | refactored presentation logic | react children property

--- app/components/popular.js
+++ app/components/popular.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { fetchPopularRepos } from '../utils/api'
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
+import Card from './card'
 
 function LanguagesNav({ selectedLanguage, onUpdateLanguage }) {
 
@@ -33,54 +34,51 @@ LanguagesNav.propTypes = {
 
 // styling
 
-function ReposGrid ( {repos} ) {
+function ReposGrid({ repos }) {
     return (
         <ul className='grid space-around'>
-            {   
-                repos.map((repo,index) => {
-                    const { name, owner, html_url, stargazers_count, forks, open_issues} = repo
-                    const {login, avatar_url} = owner
-                    
+            {
+                repos.map((repo, index) => {
+                    const { name, owner, html_url, stargazers_count, forks, open_issues } = repo
+                    const { login, avatar_url } = owner
+
                     return (
-                        <li key={html_url} className='card bg-light'>
-                            <h4 className='header-lg center-text'>
-                                #{index+1}
-                            </h4>
-                            <img className ='avatar'
-                                 src={avatar_url}
-                                 alt={`Avatar for ${login}`}
+                        <li key={html_url} >
+
+                            <Card
+                                header={`#${index + 1}`}
+                                avatar={avatar_url}
+                                href={html_url}
+                                name={login}
                             >
-                            </img>
+                                <ul className='card-list'>
+                                    <li>
+                                        <FaUser color='rgb(255,191, 116)' />
+                                        <a href={`https://github.com/${login}`}>
+                                            {login}
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <FaStar color='rgb(255,215, 0)' size={22} />
+                                        {stargazers_count.toLocaleString()} stars
+                                </li>
+                                    <li>
+                                        <FaCodeBranch color='rgb(129,195, 245)' size={22} />
+                                        {forks.toLocaleString()} forks
 
-                            <h2 className='center-text'>
-                                <a className='link' href={html_url}>{login}</a>
-                            </h2>
+                                </li>
+                                    <li>
+                                        <FaExclamationTriangle color='rgb(255,138, 147)' size={22} />
+                                        {open_issues.toLocaleString()} open issues
+                                </li>
+                                </ul>
+                            </Card>
 
-                            <ul className='card-list'>
-                                <li>
-                                    <FaUser color='rgb(255,191, 116)'/>
-                                    <a href={`https://github.com/${login}`}>
-                                        {login}
-                                    </a>
-                                </li>
-                                <li>
-                                    <FaStar color='rgb(255,215, 0)' size={22}/>
-                                    {stargazers_count.toLocaleString()} stars
-                                </li>
-                                <li>
-                                    <FaCodeBranch color='rgb(129,195, 245)' size={22}/>
-                                    {forks.toLocaleString()} forks
-                                    
-                                </li>
-                                <li>
-                                    <FaExclamationTriangle color='rgb(255,138, 147)' size={22}/>
-                                    {open_issues.toLocaleString()} open issues
-                                </li>
-                            </ul>
+
 
                         </li>
                     )
-                
+
                 })
             }
 
@@ -133,63 +131,63 @@ export default class Popular extends React.Component {
                     )
 
                 })
-            }
-        
+        }
 
-                    //     fetchPopularRepos(selectedLanguage)
-                    // .then((repos) => {
-                    //     this.setState({
-                    //         repos,
-                    //         error:null
-                    //     })
-                    // })
-                    // .catch((error) => {
-                    //     console.warn('Error while fetching', error)
-                    //     this.setState(
-                    //         {error: 'there is an error'}
-                    //     )
-                    // })
 
-                
+        //     fetchPopularRepos(selectedLanguage)
+        // .then((repos) => {
+        //     this.setState({
+        //         repos,
+        //         error:null
+        //     })
+        // })
+        // .catch((error) => {
+        //     console.warn('Error while fetching', error)
+        //     this.setState(
+        //         {error: 'there is an error'}
+        //     )
+        // })
 
-    
+
+
+
 
 
     }
 
-        componentDidMount(){
-            this.updateLanguage(this.state.selectedLanguage)
-        }
-
-
-        isLoading() {
-
-            const { selectedLanguage, repos, error} = this.state
-
-            return !repos[selectedLanguage] && error === null
-
-           // return this.state.repos === null && this.state.error === null
-        }
-
-
-        render() {
-
-            const { selectedLanguage, repos, error } = this.state
-            return (
-
-                <React.Fragment>
-                    <LanguagesNav
-                        selectedLanguage={selectedLanguage}
-                        onUpdateLanguage={this.updateLanguage}
-                    />
-
-                    {this.isLoading() && <p>Loading..</p>}
-
-                    {error && <p className='center-text error'>{error}</p>}
-
-                    {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]}/>}
-                </React.Fragment>
-            )
-
-        }
+    componentDidMount() {
+        this.updateLanguage(this.state.selectedLanguage)
     }
+
+
+    isLoading() {
+
+        const { selectedLanguage, repos, error } = this.state
+
+        return !repos[selectedLanguage] && error === null
+
+        // return this.state.repos === null && this.state.error === null
+    }
+
+
+    render() {
+
+        const { selectedLanguage, repos, error } = this.state
+        return (
+
+            <React.Fragment>
+                <LanguagesNav
+                    selectedLanguage={selectedLanguage}
+                    onUpdateLanguage={this.updateLanguage}
+                />
+
+                {this.isLoading() && <p>Loading..</p>}
+
+                {error && <p className='center-text error'>{error}</p>}
+
+                {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
+            </React.Fragment>
+        )
+
+    }
+}
