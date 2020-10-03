f92e3f9f736e434f4ce9b28f30e160a48171e19d
Gaurav Bajpai <gaurav.tcs15@gmail.com>
2020-10-03 11:23:39 +0530

    results-component ui completed

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,25 +1,133 @@
 import React from 'react'
 import { battle } from '../utils/api'
-
+import {FaCompass , FaBriefcase, FaUsers, FaUserFriends, FaUser} from 'react-icons/fa'
 
 export default class Results extends React.Component {
 
-    componentDidMount() {
-        const { playerOne, playerTwo} = this.props
+    constructor(props) {
+        super(props)
 
-        battle([playerOne,playerTwo]).then
-        ((players) => {
-            console.log(players)
-        })
+        this.state = {
+            winner: null,
+            loser: null,
+            error: null,
+            loading: true
+        }
+    }
+
+    componentDidMount() {
+        const { playerOne, playerTwo } = this.props
+
+        
+
+        battle([playerOne, playerTwo]).then
+            ((players) => {
+                this.setState({
+                    winner: players[0],
+                    loser: players[1],
+                    error: null,
+                    loading: false
+                })
+            }).catch(({ message }) => {
+                this.setState({
+                    error: message,
+                    loading: false
+                })
+            })
 
     }
 
-    render () {
-        return (
-            <div>
-                Results 
-                <pre> { JSON.stringify(this.props,null,2) }</pre>
-            </div>
-        )
+    render() {
+
+        const { winner, loser, error, loading } = this.state
+
+        if (error) {
+            return <p className='center-text error'>{error}</p>
+        }
+
+        if (loading === true) {
+            return <p >Loading</p>
+        }
+
+        if(!loading) {
+            return (
+                <div className='grid space-around container-sm'>
+                    <div className='card bg-light'>
+                        <h4 className='header-lg center-text'>
+                            {winner.score === loser.score ? 'Tie' : 'Winner'}
+                        </h4>
+                        <img className='avatar'
+                            src={winner.profile.avatar_url}
+                            alt={`Avatar for ${winner.profile.login}`} />
+                        <h4 className='center-text'>Score : {winner.score.toLocaleString()}</h4>
+                        <h2 className='center-text'>
+                            <a className='link' href={winner.profile.html_url}>
+                                {winner.profile.login}
+                            </a>
+                        </h2>
+                        <ul className='card-list'>
+                            <li>
+                                <FaUser color='rgb(239,115,115)' size={22} />
+                                {winner.profile.name}
+                            </li>
+                            <li>
+                                <FaCompass color='rgb(114,115,255)' size={22} />
+                                {winner.profile.location}
+                            </li>
+                            <li>
+                                <FaBriefcase color='#795548' size={22} />
+                                {winner.profile.company}
+                            </li>
+                            <li>
+                                <FaUsers color='rgb(129,195, 245)' size={22} />
+                                {winner.profile.followers.toLocaleString()} followers
+                            </li>
+                            <li>
+                                <FaUserFriends color='rgb(64,183,95)' size={22} />
+                                {winner.profile.following.toLocaleString()} following
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='card bg-light'>
+                        <h4 className='header-lg center-text'>
+                            {winner.score === loser.score ? 'Tie' : 'Loser'}
+                        </h4>
+                        <img className='avatar'
+                            src={loser.profile.avatar_url}
+                            alt={`Avatar for ${winner.profile.login}`} />
+                        <h4 className='center-text'>Score : {loser.score.toLocaleString()}</h4>
+                        <h2 className='center-text'>
+                            <a className='link' href={loser.profile.html_url}>
+                                {loser.profile.login}
+                            </a>
+                        </h2>
+                        <ul className='card-list'>
+                            <li>
+                                <FaUser color='rgb(239,115,115)' size={22} />
+                                {loser.profile.name}
+                            </li>
+                            <li>
+                                <FaCompass color='rgb(114,115,255)' size={22} />
+                                {loser.profile.location}
+                            </li>
+                            <li>
+                                <FaBriefcase color='#795548' size={22} />
+                                {loser.profile.company}
+                            </li>
+                            <li>
+                                <FaUsers color='rgb(129,195, 245)' size={22} />
+                                {loser.profile.followers.toLocaleString()} followers
+                            </li>
+                            <li>
+                                <FaUserFriends color='rgb(64,183,95)' size={22} />
+                                {loser.profile.following.toLocaleString()} following
+                            </li>
+                        </ul>
+                    </div>
+                    
+                </div>
+            )
+        }
+        
     }
 }
