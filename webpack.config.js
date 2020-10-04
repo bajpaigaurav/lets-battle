c6659a39cb70232b744210d15529e1a3d96ce7ec
Gaurav Bajpai <gaurav.tcs15@gmail.com>
2020-10-04 23:07:37 +0530

    added copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: './app/App.js',
@@ -21,7 +22,11 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin( {
             template: 'app/index.html'
-        })
+        }),
+
+    new CopyPlugin([
+        { from : '404.html' }
+      ])
     ],
     devServer: {
         historyApiFallback: true
